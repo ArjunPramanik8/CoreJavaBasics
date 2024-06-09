595b93105d83f252d5535ebb64754074bc29838c
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-06-09 17:41:03 +0530

    First commit

--- src/Core_Fundamentals/Day_1/Hello_World.java
+++ src/Core_Fundamentals/Day_1/Hello_World.java
@@ -2,6 +2,6 @@ package Core_Fundamentals.Day_1;
 
 public class Hello_World {
     public static void main(String[] args) {
-        System.out.println("Hello Java Learners");
+        System.out.println(" Hello Java Learners ");
     }
 }
