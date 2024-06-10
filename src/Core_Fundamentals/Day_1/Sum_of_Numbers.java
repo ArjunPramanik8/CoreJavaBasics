874aafe7c2a55d0f4c34fe6af6ec9e6af18aa386
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-06-10 23:47:45 +0530

    Second Day Commit

---
+++ src/Core_Fundamentals/Day_1/Sum_of_Numbers.java
@@ -0,0 +1,12 @@
+package Core_Fundamentals.Day_1;
+
+public class Sum_of_Numbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        // Calculate and print the sum
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
