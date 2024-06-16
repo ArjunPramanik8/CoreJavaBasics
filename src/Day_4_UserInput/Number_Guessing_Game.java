015ccd90f6cbb5a61bc445efd6a389d909ada882
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-06-16 23:26:46 +0530

    Third Day Commit

---
+++ src/Day_4_UserInput/Number_Guessing_Game.java
@@ -0,0 +1,28 @@
+package Day_4_UserInput;
+
+import java.util.Scanner;
+
+public class Number_Guessing_Game
+{
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int targetNumber = 10; //Change this for different difficulty
+
+        System.out.println("Please Guess your number between 1 to 20");
+        int guess = sc.nextInt();
+
+        if(guess == targetNumber)
+        {
+            System.out.println("Congratulations! You guessed the number.");
+        }
+        else{
+            System.out.println("Sorry! Incorrect guess. Please try again!");
+        }
+
+        sc.close();
+
+    }
+}
