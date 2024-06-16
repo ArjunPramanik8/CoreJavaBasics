6ae0816c6087095597a1827214fe88e7119a616a
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-06-16 23:29:49 +0530

    Fourth Day Commit

--- src/Day_4_UserInput/Name_Greeter.java
+++ src/Day_4_UserInput/Name_Greeter.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Name_Greeter {
     public static void main(String[] args) {
+
         Scanner sc= new Scanner(System.in);
         System.out.println("Please enter you name");
         String name = sc.nextLine();
