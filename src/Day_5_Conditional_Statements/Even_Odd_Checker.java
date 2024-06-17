8d84f02e4011e8fa15c71b7ae6c3f0edc84eb99e
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-06-17 23:39:14 +0530

    Fifth Day Commit

---
+++ src/Day_5_Conditional_Statements/Even_Odd_Checker.java
@@ -0,0 +1,25 @@
+package Day_5_Conditional_Statements;
+import java.util.Scanner;
+
+public class Even_Odd_Checker 
+{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter the number");
+        int Number = sc.nextInt();
+
+        if(Number%2==0)
+        {
+            System.out.println(Number + " is Even");
+        }
+        else
+        {
+            System.out.println(Number + " is Odd");
+        }
+
+        sc.close();
+    }
+
+}
