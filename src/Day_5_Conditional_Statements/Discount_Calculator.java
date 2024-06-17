58be5764704cf0d30b454cf07244d06ace0326b3
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-06-17 23:48:15 +0530

    Fifth Day Commit

---
+++ src/Day_5_Conditional_Statements/Discount_Calculator.java
@@ -0,0 +1,24 @@
+package Day_5_Conditional_Statements;
+import java.util.Scanner;
+
+public class Discount_Calculator
+{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Original Price: ");
+        double price = sc.nextDouble();
+
+        System.out.println("Enter the discount percentage (0-100): ");
+        double discount = sc.nextDouble();
+
+        double discount_amount= price * (discount/100);
+        double final_Price= price-discount_amount;
+        System.out.println("Final Price After Discount: RS"+ final_Price);
+
+        sc.close();
+    }
+
+
+}
