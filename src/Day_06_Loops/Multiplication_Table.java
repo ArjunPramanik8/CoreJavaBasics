65f13fd7847e85e35d24a4a305f610cc1c9a5660
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-06-19 22:53:35 +0530

    Sixth Day Commit

---
+++ src/Day_06_Loops/Multiplication_Table.java
@@ -0,0 +1,18 @@
+package Day_06_Loops;
+
+import java.sql.SQLOutput;
+
+public class Multiplication_Table
+{
+    public static void main(String[] args) {
+
+        int number =5; //change this for a different table
+
+        //Loop to print multiplication table from 1 to 10
+        for (int i = 0; i <= 10; i++)
+        {
+            int product= number * i;
+            System.out.println(number +"*"+ i +"="+ product);
+        }
+    }
+}
