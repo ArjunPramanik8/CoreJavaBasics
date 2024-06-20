9bd7d601d2df4a47f3103c6e10abe4becad41d10
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-06-20 23:16:52 +0530

    Seventh Day Commit

---
+++ src/Day_07_Arrays/String_Array_Example.java
@@ -0,0 +1,15 @@
+package Day_07_Arrays;
+
+public class String_Array_Example
+{
+    public static void main(String[] args) {
+
+        String[] fruits={"Apple","Mango","Banana","Orange","Grapes"};
+
+        for(int i = 0; i < fruits.length; i++)
+        {
+            System.out.println(fruits[i]);
+
+        }
+    }
+}
