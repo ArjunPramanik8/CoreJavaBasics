1501467ad3b3a17243d48e1e9015e634405be033
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-06-27 00:04:30 +0530

    Eight Day Commit

---
+++ src/Day_8_Strings_Text_Manipulation/String_Manipulation.java
@@ -0,0 +1,15 @@
+package Day_8_Strings_Text_Manipulation;
+
+public class String_Manipulation {
+    public static void main(String[] args) {
+
+        String sentence="We are coding";
+
+        System.out.println(sentence.toUpperCase());
+        System.out.println(sentence.toLowerCase());
+
+        String Sentence2 = sentence.substring(0,6);
+        System.out.println(Sentence2 );
+
+    }
+}
