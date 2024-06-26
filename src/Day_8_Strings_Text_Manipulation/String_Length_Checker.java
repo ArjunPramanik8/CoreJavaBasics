1a0e27997619cea7fa05c04b473af0c8ad0b3478
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-06-27 00:00:14 +0530

    Eight Day Commit

---
+++ src/Day_8_Strings_Text_Manipulation/String_Length_Checker.java
@@ -0,0 +1,18 @@
+package Day_8_Strings_Text_Manipulation;
+
+import org.w3c.dom.ls.LSOutput;
+public class String_Length_Checker
+{
+public static void main(String[] args) {
+
+
+        String name="Swami Vivekananda";
+        // fetching the length : counting the number of characters
+        int l= name.length();
+    System.out.println("The length of the string is"+ l);
+
+    }
+}
+
+
+
