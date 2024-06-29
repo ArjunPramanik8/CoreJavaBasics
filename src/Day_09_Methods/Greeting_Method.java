78503bfd12b6bcaa3bb723e2b2b8dbc3e86fea2f
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-06-29 22:29:26 +0530

    Nineth Day Commit

---
+++ src/Day_09_Methods/Greeting_Method.java
@@ -0,0 +1,13 @@
+package Day_09_Methods;
+
+public class Greeting_Method
+{
+    public static void greet(String name)
+    {
+        System.out.println("Hello,"+ name + "!");
+    }
+
+    public static void main(String[] args) {
+        greet("Hare Krishna");
+    }
+}
