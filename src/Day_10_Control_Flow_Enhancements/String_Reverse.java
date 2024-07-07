df82dad9de5805aeac99a9699124214735867506
ArjunPramanik8 <pramanik.arjun04@gmail.com>
2024-07-08 00:02:28 +0530

    Tenth Day Commit

---
+++ src/Day_10_Control_Flow_Enhancements/String_Reverse.java
@@ -0,0 +1,14 @@
+package Day_10_Control_Flow_Enhancements;
+
+public class String_Reverse
+{
+    public static void main(String[] args) {
+
+    String s="Accenture";
+    StringBuffer sf = new StringBuffer(s);
+
+        System.out.println(sf.reverse());
+
+    }
+
+}
