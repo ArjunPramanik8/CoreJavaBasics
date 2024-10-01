6a2a47bdf6b2e5386fb959343d8a35882cb84ce6
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-01 20:45:25 +0530

    Update Product_Details.java

--- src/Day_12_String_Formatting/Product_Details.java
+++ src/Day_12_String_Formatting/Product_Details.java
@@ -8,6 +8,7 @@ public class Product_Details {
         double price = 49.99;
         int quantity = 2;
 
+    
         // Format output string with placeholders and formatting
         String productDetails = String.format("Product: %s\nPrice: $%.2f\nQuantity: %d",
                 name, price, quantity);
