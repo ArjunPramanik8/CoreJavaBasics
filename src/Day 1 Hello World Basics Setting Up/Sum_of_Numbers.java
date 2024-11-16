0316e5b59509229ee5e13b8f3e51e83fa9f30640
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-16 23:18:08 +0530

    Update Sum_of_Numbers.java

--- src/Day 1 Hello World Basics Setting Up/Sum_of_Numbers.java
+++ src/Day 1 Hello World Basics Setting Up/Sum_of_Numbers.java
@@ -4,7 +4,6 @@ public class Sum_of_Numbers {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 20;
-
         // Calculate and print the sum
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
