59e86422c7f289dba55461ac1c74b5e19ea617d8
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2025-01-19 14:39:57 +0530

    Update Age_Calculator.java

--- src/Day 1 Hello World Basics Setting Up/Age_Calculator.java
+++ src/Day 1 Hello World Basics Setting Up/Age_Calculator.java
@@ -4,6 +4,7 @@ public class Age_Calculator {
     public static void main(String[] args) 
     {
         int birthYear = 1990; // Year of birth
+        
         // Calculate current age (assuming year is 2024)
         int currentYear = 2024;
         int age = currentYear - birthYear;
