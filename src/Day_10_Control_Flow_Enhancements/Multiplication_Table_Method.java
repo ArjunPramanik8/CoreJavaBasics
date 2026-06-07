a3853806f4a7b5c4d00e9e1609670eb750e8ba46
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2026-06-07 07:59:47 +0530

    Update Multiplication_Table_Method.java

--- src/Day_10_Control_Flow_Enhancements/Multiplication_Table_Method.java
+++ src/Day_10_Control_Flow_Enhancements/Multiplication_Table_Method.java
@@ -5,7 +5,6 @@ public class Multiplication_Table_Method {
     public static void main(String[] args) {
         printMultiplicationTable(5); // Call the method with an argument
     }
-
     public static void printMultiplicationTable(int number) {
         for (int i = 1; i <= 10; i++) {
             int product = number * i;
