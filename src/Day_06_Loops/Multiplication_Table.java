8f9f90d19ff62e92922c7c56b862bca10148bf4a
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2026-06-07 07:57:44 +0530

    Update Multiplication_Table.java

--- src/Day_06_Loops/Multiplication_Table.java
+++ src/Day_06_Loops/Multiplication_Table.java
@@ -12,7 +12,7 @@ public class Multiplication_Table
         {
             int product= number * i;
             System.out.println(number +"*"+ i +"="+ product);
-        
+    
         }
     }
 }
