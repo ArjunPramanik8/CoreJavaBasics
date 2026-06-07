d14aa3a468f931f1d78eb519d96f21bc0ca79135
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2026-06-07 07:59:00 +0530

    Update Area_Calculator_Method.java

--- src/Day_09_Methods/Area_Calculator_Method.java
+++ src/Day_09_Methods/Area_Calculator_Method.java
@@ -8,7 +8,6 @@ public class Area_Calculator_Method {
     }
 
     public static void main(String[] args) {
-
         double Area= calculateArea(20,5);
         System.out.println("Area= "+ Area );
     }
