ba207891216919493802876caaa52e46a2f0bdce
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2026-06-11 06:43:06 +0530

    Update Multiplication_Table.java

--- src/Day_06_Loops/Multiplication_Table.java
+++ src/Day_06_Loops/Multiplication_Table.java
@@ -6,7 +6,6 @@ public class Multiplication_Table
     public static void main(String[] args) {
 
         int number =5; //change this for a different table
-
         //Loop to print multiplication table from 1 to 10
         for (int i = 0; i <= 10; i++)
         {
