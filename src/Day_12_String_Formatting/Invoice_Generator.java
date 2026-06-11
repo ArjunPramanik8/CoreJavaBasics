afa9797a5717c1c83a9981b23cd97be052287046
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2026-06-11 06:44:39 +0530

    Update Invoice_Generator.java

--- src/Day_12_String_Formatting/Invoice_Generator.java
+++ src/Day_12_String_Formatting/Invoice_Generator.java
@@ -20,6 +20,7 @@ public class Invoice_Generator {
             System.out.println(items[i] + "\t$" + prices[i] + "\t" + quantities[i] + "\t$" + String.format("%.2f", itemTotal));
             
         }
+        
         System.out.println("----------------------------------");
         System.out.println("Total:\t\t\t$" + String.format("%.2f", total));
     }
